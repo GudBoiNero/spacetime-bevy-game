6ee076de50ce239f69dd9f3638fdeaa97f9180bd
Ben Champagne <donkeykong797@gmail.com>
2024-01-11 11:47:57 -0500

    boiler

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -1,4 +1,36 @@
-use spacetimedb::{spacetimedb};
+use spacetimedb::{spacetimedb, Identity, SpacetimeType, spacetimedb_lib::primary_key};
+
+#[spacetimedb(table)]
+pub struct User {
+    #[primarykey]
+    pub id: Identity,
+    pub player_id: u64,
+    pub connected: bool
+}
+
+#[derive(SpacetimeType, Clone)]
+pub struct StdbVector2 {
+    pub x: f32,
+    pub y: f32,
+}
+
+#[spacetimedb(table)]
+#[derive(Clone)]
+pub struct Object {
+    #[primarykey]
+    #[autoinc]
+    pub object_id: u64,
+    pub position: StdbVector2,
+    pub velocity: StdbVector2,
+}
+
+#[spacetimedb(table)]
+#[derive(Clone)]
+pub struct Player {
+    #[primarykey]
+    #[autoinc]
+    pub player_id: u64
+}
 
 #[spacetimedb(init)]
 pub fn init() {

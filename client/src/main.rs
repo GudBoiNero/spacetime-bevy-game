3b6dbf892408eb85226d6fd85b682a76415e88d9
Ben Champagne <donkeykong797@gmail.com>
2024-01-12 10:01:23 -0500

    boiler

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,11 +1,39 @@
 use bevy::prelude::*;
 
 fn main() {
+    register_callbacks();
+    connect_to_db();
+    subscribe_to_tables();
+    user_input_loop();
+
     App::new()
-    .add_plugins(DefaultPlugins)
-    .add_systems(Startup, init)
-    .run();
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, init)
+        .add_systems(Update, update)
+        .run();
 }
 
 
-fn init() {}
+fn init() {
+
+}
+
+fn update() {
+
+}
+
+fn register_callbacks() {
+
+}
+
+fn connect_to_db() {
+
+}
+
+fn subscribe_to_tables() {
+
+}
+
+fn user_input_loop() {
+    
+}

d69da19a744944ae5012a8cbdd0029b805170c2c
Ben Champagne <donkeykong797@gmail.com>
2024-01-13 15:13:05 -0500

    more

--- client/src/components/player.rs
+++ client/src/components/player.rs
@@ -2,14 +2,9 @@ use bevy::prelude::*;
 
 use crate::util::vector2::Vector2;
 
-#[derive(Component, Clone, Copy)]
+#[derive(Component, Clone)]
 pub struct Player {
     pub position: Vector2,
-    pub velocity: Vector2
-}
-
-impl Default for Player {
-    fn default() -> Self {
-        Self { velocity: Vector2 { x: 0.0, y: 1.0 }, position: Vector2 { x: 0.0, y: 0.0 } }
-    }
+    pub velocity: Vector2,
+    pub sprite: SpriteBundle
 }

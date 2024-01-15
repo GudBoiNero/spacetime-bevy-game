9d62ec47b05670b49cc6ad0d363fe53b73452710
Ben Champagne <donkeykong797@gmail.com>
2024-01-15 17:36:55 -0500

    rust is wild

--- client/src/main.rs
+++ client/src/main.rs
@@ -19,17 +19,28 @@ const DB_NAME: &str = "spacetime-bevy-game";
 const CREDS_DIR: &str = ".spacetime-bevy-game";
 
 /// Unbound Callback Enum
+/// Used to tell our unbounded reciever what \
+/// specific event has occured while passing params.
+/// [System based on this](https://github.com/clockworklabs/SpacetimeDB/blob/master/crates/sdk/examples/cursive-chat/main.rs#L45)
 enum Uncb {
-
+    PlayerUpdate {
+        id: Identity
+    },
+    PlayerInsert {
+        id: Identity
+    },
+    PlayerDelete {
+        id: Identity
+    }
 }
 
-type UncbSend = mpsc::Sender<Uncb>;
-type UncbRecv = mpsc::Receiver<Uncb>;
+type UncbSend = mpsc::UnboundedSender<Uncb>;
+type UncbRecv = mpsc::UnboundedReceiver<Uncb>;
 
 fn main() {
     let (uncb_send, uncb_recv) = mpsc::unbounded::<Uncb>();
 
-    register_callbacks();
+    register_callbacks(uncb_send);
     connect_to_db();
     subscribe_to_tables();
 
@@ -62,15 +73,11 @@ fn subscribe_to_tables() {
 //#endregion subscribers
 
 //#region callbacks
-fn register_callbacks() {
+fn register_callbacks(uncb_send: UncbSend) {
     once_on_connect(on_connected);
     on_disconnect(on_disconnected);
     
-    StdbClient::on_insert(on_client_inserted);
-    StdbClient::on_update(on_client_updated);
 
-    StdbPlayer::on_insert(on_player_inserted);
-    StdbPlayer::on_update(on_player_updated);
 }
 
 fn on_connected(creds: &Credentials, _client_address: Address) {
@@ -84,27 +91,35 @@ fn on_disconnected() {
     std::process::exit(0)
 }
 
-fn on_client_inserted(client: &StdbClient, _: Option<&ReducerEvent>) {
-    if client.connected {
-        println!("Client {} connected.", identity_leading_hex(&client.client_id));
+fn on_client_inserted(uncb_send: UncbSend) -> impl FnMut(&StdbClient, Option<&ReducerEvent>) + Send + 'static {
+    move |client, event| { 
+        if client.connected {
+            println!("Client {} connected.", identity_leading_hex(&client.client_id));
+        }
     }
 }
 
-fn on_client_updated(old: &StdbClient, new: &StdbClient, _: Option<&ReducerEvent>) {
-    if old.connected && !new.connected {
-        println!("User {} disconnected.", identity_leading_hex(&new.client_id));
-    }
-    if !old.connected && new.connected {
-        println!("User {} connected.", identity_leading_hex(&new.client_id));
+fn on_client_updated(uncb_send: UncbSend) -> impl FnMut(&StdbClient, &StdbClient, Option<&ReducerEvent>) + Send + 'static {
+    move |old, new, event| {
+        if old.connected && !new.connected {
+            println!("User {} disconnected.", identity_leading_hex(&new.client_id));
+        }
+        if !old.connected && new.connected {
+            println!("User {} connected.", identity_leading_hex(&new.client_id));
+        }
     }
 }
 
-fn on_player_inserted(player: &StdbPlayer, _: Option<&ReducerEvent>) {
-    
-}
+fn on_player_inserted(uncb_send: UncbSend) -> impl FnMut(&StdbPlayer, Option<&ReducerEvent>) + Send + 'static {
+    move |player, event| {
 
-fn on_player_updated(old: &StdbPlayer, new: &StdbPlayer, _: Option<&ReducerEvent>) {
+    } 
+}  
 
+fn on_player_updated(uncb_send: UncbSend) -> impl FnMut(&StdbPlayer, &StdbPlayer, Option<&ReducerEvent>) + Send + 'static  {
+    move |old, new, event| {
+        
+    } 
 }
 
 fn identity_leading_hex(id: &Identity) -> String {

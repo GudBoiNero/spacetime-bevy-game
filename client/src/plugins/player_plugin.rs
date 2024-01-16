53363bc48aaed14f78a00597aaa6e4b5b28bafeb
Ben Champagne <donkeykong797@gmail.com>
2024-01-15 20:56:41 -0500

    removed param

--- client/src/plugins/player_plugin.rs
+++ client/src/plugins/player_plugin.rs
@@ -13,7 +13,7 @@ impl Plugin for PlayerPlugin {
 /// Grabs all `StdbPlayer`s from the database and spawns \
 /// a player for them. If the player has the same `Identity` \
 /// as the client, initialize it's bundle with input controls.
-fn init_players(c: Commands, recv: Res<UncbReceiver>) {
+fn init_players(c: Commands) {
     
 }
 
